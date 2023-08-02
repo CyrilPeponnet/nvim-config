4f3c031270c3ab06196a602acff05029729d70d0
CyrilPeponnet <cyril@peponnet.fr>
2023-08-02 08:46:06 -0700

    Adding helm_ls and remove yaml diags

--- lua/plugins/lang/helm.lua
+++ lua/plugins/lang/helm.lua
@@ -1,10 +1,9 @@
--- detach yamlls for helm templating format
+-- detach lsp for helm files
 local function detach_yamlls()
 	local clients = vim.lsp.get_active_clients()
 	for client_id, client in pairs(clients) do
 		if client.name == "yamlls" or client.name == "docker_compose_language_service" then
 			vim.lsp.buf_detach_client(0, client_id)
-			vim.diagnostic.disable(nil, vim.lsp.diagnostic.get_namespace(client.id))
 		end
 	end
 end
@@ -26,10 +25,36 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- filter the diagnostic we dont want
+local function filter_diagnostics(diagnostic)
+	-- if we deal with a helm file we filter out the yamlls and docker_compose_language_service
+	if vim.bo.filetype == "helm" then
+		-- somehow the source is nil for some so we filter out all
+		if diagnostic.source == nil or diagnostic.source == "YAML" then
+			return false
+		end
+	end
+	return true
+end
+
+-- override the default diagnostic
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(function(_, result, ctx, config)
+	result.diagnostics = vim.tbl_filter(filter_diagnostics, result.diagnostics)
+	vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+end, {})
+
 return {
 
 	{
 		"towolf/vim-helm",
 		event = "VeryLazy",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				helm_ls = {},
+			},
+		},
 	},
 }
